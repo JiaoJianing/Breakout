c2c70e4b91ffba5d0d103b9d915f72f26f904dcd
jiaojn <jiaojn@skong.com>
2018-04-02 10:15:22 +0800

    添加天空盒

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -130,6 +130,33 @@ unsigned int loadTexture(char const *path)
 	return textureID;
 }
 
+unsigned int loadCubeMap(std::vector<std::string> faces) {
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+	
+	int width, height, nrChannels;
+	for (int i = 0; i < faces.size(); i++) {
+		unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data) {
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+			stbi_image_free(data);
+		}
+		else {
+			std::cout << "Cube Map failed to load at path: " << faces[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	return textureID;
+}
+
 int main(int argc, char** argv) {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -152,11 +179,6 @@ int main(int argc, char** argv) {
 	glViewport(0, 0, screenWidth, screenHeight);
 
 	glEnable(GL_DEPTH_TEST);
-	//glDepthFunc(GL_ALWAYS);
-	glDepthFunc(GL_LESS);
-
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetCursorPosCallback(window, mouse_move_callback);//鼠标移动
@@ -209,47 +231,50 @@ int main(int argc, char** argv) {
 		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f, // top-left
 		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f  // bottom-left      
 	};
-	float planeVertices[] = {
-		//位置               纹理坐标，设成了2,配合GL_REPEAT
-		5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
-		-5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
-		-5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
 
-		5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
--5.0f, -0.5f, -5.0f, 0.0f, 2.0f,
-5.0f, -0.5f, -5.0f, 2.0f, 2.0f
-	};
-	float grassVertices[] = {
-		//位置             纹理坐标
-		-0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
-		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
-		0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
-		0.5f, -0.5, 0.0f, 1.0f, 0.0f,
-		0.5f, 0.5f, 0.0f, 1.0f, 1.0f,
-		-0.5f, 0.5f, 0.0f, 0.0f, 1.0f
-	};
-	float fullScreenVertices[] = {
-		//位置             纹理坐标
-		-1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-		1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-		1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-		1.0f, 1.0f, 0.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 0.0f, 0.0f, 1.0f
-	};
+	float skyboxVertices[] = {
+		-1.0f,  1.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
 
-	//窗户位置
-	std::vector<glm::vec3> windowPositions;
-	windowPositions.push_back(glm::vec3(-1.5f, 0.0f, -0.48f));
-	windowPositions.push_back(glm::vec3(1.5f, 0.0f, 0.61f));
-	windowPositions.push_back(glm::vec3(0.0f, 0.0f, 0.7f));
-	windowPositions.push_back(glm::vec3(-0.3f, 0.0f, -2.3f));
-	windowPositions.push_back(glm::vec3(0.5f, 0.0f, -0.6f));
-	//草丛位置
-	std::vector<glm::vec3> grassPositions;
-	grassPositions.push_back(glm::vec3(-2.5f, 0.0f, -0.3f));
-	grassPositions.push_back(glm::vec3(0.5f, 0.0f, 0.41f));
-	grassPositions.push_back(glm::vec3(1.0f, 0.0f, 0.6f));
+		-1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+
+		-1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,
+
+		-1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f, -1.0f,
+		1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f,
+
+		-1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f,
+		-1.0f, -1.0f,  1.0f,
+		1.0f, -1.0f,  1.0f
+	};
 
 	// 立方体 VAO
 	unsigned int cubeVAO, cubeVBO;
@@ -263,104 +288,37 @@ int main(int argc, char** argv) {
 	glEnableVertexAttribArray(1);
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glBindVertexArray(0);
-	// 地面 VAO
-	unsigned int planeVAO, planeVBO;
-	glGenVertexArrays(1, &planeVAO);
-	glGenBuffers(1, &planeVBO);
-	glBindVertexArray(planeVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
+	//天空盒VAO
+	unsigned int skyboxVAO, skyboxVBO;
+	glGenVertexArrays(1, &skyboxVAO);
+	glGenBuffers(1, &skyboxVBO);
+	glBindVertexArray(skyboxVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindVertexArray(0);
-	//窗户 VAO
-	unsigned int windowVAO, windowVBO;
-	glGenVertexArrays(1, &windowVAO);
-	glGenBuffers(1, &windowVBO);
-	glBindVertexArray(windowVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, windowVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(grassVertices), &grassVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindVertexArray(0);
-	//草丛 VAO
-	unsigned int grassVAO, grassVBO;
-	glGenVertexArrays(1, &grassVAO);
-	glGenBuffers(1, &grassVBO);
-	glBindVertexArray(grassVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, grassVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(grassVertices), &grassVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindVertexArray(0);
-	//全屏 VAO
-	unsigned int fullScreenVAO, fullScreenVBO;
-	glGenVertexArrays(1, &fullScreenVAO);
-	glGenBuffers(1, &fullScreenVBO);
-	glBindVertexArray(fullScreenVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, fullScreenVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(fullScreenVertices), &fullScreenVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glBindVertexArray(0);
-
-#pragma region 配置帧缓冲
-	unsigned int framebuffer;
-	glGenFramebuffers(1, &framebuffer);
-	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-	//生成纹理
-	unsigned int texColorBuffer;
-	glGenTextures(1, &texColorBuffer);
-	glBindTexture(GL_TEXTURE_2D, texColorBuffer);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, screenWidth, screenHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	//附加到帧缓冲的颜色附件
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
-
-	//生成深度、模板渲染缓冲
-	unsigned int rbo;
-	glGenRenderbuffers(1, &rbo);
-	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, screenWidth, screenHeight);
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-	//附加到帧缓冲的深度、模板附件
-	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
-
-	//检查帧缓冲完整性
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-		std::cout << "FrameBuffer not completed!" << std::endl;
-	}
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-#pragma endregion
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 
 	// 加载纹理
 	//翻转纹理y坐标
-	stbi_set_flip_vertically_on_load(true);
 	unsigned int cubeTexture = loadTexture("resources/marble.jpg");
-	unsigned int floorTexture = loadTexture("resources/metal.png");
-	unsigned int windowTexture = loadTexture("resources/blending_transparent_window.png");
-	unsigned int grassTexture = loadTexture("resources/grass.png");
+	std::vector<std::string> faces
+	{
+		"resources/skybox/right.jpg",
+		"resources/skybox/left.jpg",
+		"resources/skybox/top.jpg",
+		"resources/skybox/bottom.jpg",
+		"resources/skybox/back.jpg",
+		"resources/skybox/front.jpg",
+	};
+	unsigned int skyboxTexture = loadCubeMap(faces);
 
 	Shader shader("shaders/depth_test.vs", "shaders/depth_test.fs");
-	Shader fullscreenShader("shaders/full_screen.vs", "shaders/full_screen.fs");
+	Shader skyboxShader("shaders/skybox.vs", "shaders/skybox.fs");
 
-	fullscreenShader.use();
-	fullscreenShader.setInt("texture1", 0);
 	shader.use();
 	shader.setInt("texture1", 0);
+	skyboxShader.use();
+	skyboxShader.setInt("skybox", 0);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -377,92 +335,34 @@ int main(int argc, char** argv) {
 		glm::mat4 view = glm::lookAt(camera.GetPos(), camera.GetPos() + camera.GetTarget(), camera.GetUp());
 		glm::mat4 projection = glm::perspective(glm::radians(camera.GetFov()), screenWidth / screenHeight, 0.1f, 100.0f);
 
-#pragma region 渲染到framebuffer
-		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-
 		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//开始渲染
-		glEnable(GL_DEPTH_TEST);
+		//画箱子
 		shader.use();
 		shader.setMatrix4fv("view", glm::value_ptr(view));
 		shader.setMatrix4fv("projection", value_ptr(projection));
-
-		//启用混合时：
-		//1. 先绘制不透明的物体 
-		//2. 对透明物体排序（对于草丛这种物体(alpha不全是1)，若看成透明物体的话，
-		//也要参加排序，否则需要在shader中进行处理，alpha小于一定阈值时舍弃,
-		//防止草丛在透明物体前边，透明物体绘制时由于深度测试被舍弃）
-		//3. 从远到近绘制透明物体
-
-		//次序无关透明度（OIT）是更好的解决方法。
-
-		// 两个立方体,背面剔除效果只对像立方体这种封闭形状有效，可以有效节省性能。
-		glEnable(GL_CULL_FACE);
-
-		glBindVertexArray(cubeVAO);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, cubeTexture);
 		model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f));
 		shader.setMatrix4fv("model", value_ptr(model));
+		glBindVertexArray(cubeVAO);
+		glBindTexture(GL_TEXTURE_2D, cubeTexture);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+
+		glDepthFunc(GL_LEQUAL);
+		skyboxShader.use();
+		view = glm::mat4(glm::mat3(view));
+		skyboxShader.setMatrix4fv("view", glm::value_ptr(view));
+		skyboxShader.setMatrix4fv("projection", value_ptr(projection));
 		model = glm::mat4();
-		model = glm::translate(model, glm::vec3(2.0f, 0.0f, 0.0f));
-		shader.setMatrix4fv("model", value_ptr(model));
+		//model = glm::scale(model, glm::vec3(5));
+		skyboxShader.setMatrix4fv("model", value_ptr(model));
+		glBindVertexArray(skyboxVAO);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, skyboxTexture);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
-
-		//像地面、2D草丛这种，应该禁用背面剔除。
-		glDisable(GL_CULL_FACE);
-		// 地面
-		glBindVertexArray(planeVAO);
-		glBindTexture(GL_TEXTURE_2D, floorTexture);
-		model = glm::mat4();
-		shader.setMatrix4fv("model", value_ptr(model));
-		glDrawArrays(GL_TRIANGLES, 0, 6);
 		glBindVertexArray(0);
-		//草丛
-		glBindVertexArray(grassVAO);
-		glBindTexture(GL_TEXTURE_2D, grassTexture);
-		for (unsigned int i = 0; i < grassPositions.size(); i++) {
-			model = glm::mat4();
-			model = glm::translate(model, grassPositions[i]);
-			shader.setMatrix4fv("model", glm::value_ptr(model));
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-		}
-		glBindVertexArray(0);
-		//窗户
-		glBindVertexArray(windowVAO);
-		glBindTexture(GL_TEXTURE_2D, windowTexture);
-		//窗户根据与观察者距离排序
-		std::map<float, glm::vec3> sorted;
-		for (unsigned int i = 0; i < windowPositions.size(); i++) {
-			float distance = glm::length(camera.GetPos() - windowPositions[i]);
-			sorted[distance] = windowPositions[i];
-		}
-
-		for (std::map<float, glm::vec3>::reverse_iterator it = sorted.rbegin(); it != sorted.rend(); it++) {
-			model = glm::mat4();
-			model = glm::translate(model, it->second);
-			shader.setMatrix4fv("model", glm::value_ptr(model));
-			glDrawArrays(GL_TRIANGLES, 0, 6);
-		}
-		glBindVertexArray(0);
-		//解绑framebuffer
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-#pragma endregion
-
-#pragma  region 渲染到默认帧缓冲
-		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glDisable(GL_DEPTH_TEST);
-		fullscreenShader.use();
-		glBindVertexArray(fullScreenVAO);
-		glBindTexture(GL_TEXTURE_2D, texColorBuffer);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glBindVertexArray(0);
-#pragma endregion
+		glDepthFunc(GL_LESS);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -470,15 +370,9 @@ int main(int argc, char** argv) {
 
 	// 清除操作
 	glDeleteVertexArrays(1, &cubeVAO);
-	glDeleteVertexArrays(1, &planeVAO);
-	glDeleteVertexArrays(1, &windowVAO);
-	glDeleteVertexArrays(1, &grassVAO);
-	glDeleteVertexArrays(1, &fullScreenVAO);
+	glDeleteVertexArrays(1, &skyboxVAO);
 	glDeleteBuffers(1, &cubeVBO);
-	glDeleteVertexArrays(1, &planeVBO);
-	glDeleteBuffers(1, &windowVBO);
-	glDeleteBuffers(1, &grassVBO);
-	glDeleteBuffers(1, &fullScreenVBO);
+	glDeleteBuffers(1, &skyboxVBO);
 
 	glfwTerminate();
 	return 0;
--- OpenGLTest/shaders/full_screen.fs
+++ OpenGLTest/shaders/full_screen.fs
@@ -19,17 +19,17 @@ void main()
 	//resultColor = vec4(average, average, average, 1.0);//灰度
 
 	//核 kernel(卷积矩阵)
-	vec2 offset[9] = vec2[](
-		vec2(-offset, offset),//左上
-		vec2(0.0f, offset),//正上
-		vec2(offset, offset),//右上
-		vec2(-offset, 0.0f),//左
-		vec2(0.0f, 0.0f),//中
-		vec2(offset, 0.0f),//右
-		vec2(-offset, -offset),//左下
-		vec2(0.0f, -offset),//正下
-		vec2(offset, -offset)//右下
-	);
+	//vec2 offset[9] = vec2[](
+		//vec2(-offset, offset),//左上
+		//vec2(0.0f, offset),//正上
+		//vec2(offset, offset),//右上
+		//vec2(-offset, 0.0f),//左
+		//vec2(0.0f, 0.0f),//中
+		//vec2(offset, 0.0f),//右
+		//vec2(-offset, -offset),//左下
+		//vec2(0.0f, -offset),//正下
+		//vec2(offset, -offset)//右下
+	//);
 
 	//锐化核
 	//float kernel[9] = float[](
@@ -46,22 +46,22 @@ void main()
 	//);
 
 	//边缘检测核
-	float kernel[9] = float[](
-		1, 1, 1,
-		1, -8, 1,
-		1, 1, 1
-	);
+	//float kernel[9] = float[](
+		//1, 1, 1,
+		//1, -8, 1,
+		//1, 1, 1
+	//);
 
-	vec3 sampleTex[9];
-	for (int i=0; i<9; i++){
-		sampleTex[i] = vec3(texture(texture1, texCoord.st + offset[i]));
-	}
-	vec3 col = vec3(0.0);
-	for (int i=0; i<9; i++){
-		col += sampleTex[i] * kernel[i];
-	}
+	//vec3 sampleTex[9];
+	//for (int i=0; i<9; i++){
+		//sampleTex[i] = vec3(texture(texture1, texCoord.st + offset[i]));
+	//}
+	//vec3 col = vec3(0.0);
+	//for (int i=0; i<9; i++){
+		//col += sampleTex[i] * kernel[i];
+	//}
 
-	resultColor = vec4(col, 1);
+	//resultColor = vec4(col, 1);
 
 	FragColor = resultColor;
 };
