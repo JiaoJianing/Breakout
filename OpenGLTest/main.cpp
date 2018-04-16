225c91e717269d295c66f0a31cb0e3a3b1f7bb7c
jiaojn <jiaojn@skong.com>
2018-04-16 10:43:41 +0800

    点阴影（万向阴影立方体贴图）

--- OpenGLTest/main.cpp
+++ OpenGLTest/main.cpp
@@ -24,14 +24,11 @@ bool blinn = true;
 Camera camera(screenWidth, screenHeight);
 Model nanosuit;
 
-glm::vec3 lightSrcPos(0.0f, 4.0f, -4.0f);
-glm::vec3 lightPos(0.0f, 4.0f, -4.0f);
-float lightRotAngle = 0;
+glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
 float modelRotAngle = 0;
 
-unsigned int lightVAO, lightVBO;
-unsigned int floorVAO, floorVBO;
-unsigned int floorTexture;
+unsigned int cubeVAO, cubeVBO;
+unsigned int cubeTexture;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
 	screenWidth = width;
@@ -179,30 +176,39 @@ unsigned int loadCubeMap(std::vector<std::string> faces) {
 }
 
 void RenderScene(Shader shader, float currentFrame) {
+	//绘制立方体房间
 	glm::mat4 model;
-	model = glm::rotate(model, glm::radians(-60.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::mat4();
+	model = glm::scale(model, glm::vec3(10.0f));
 	shader.setMatrix4("model", model);
-
-	glBindVertexArray(floorVAO);
+	glDisable(GL_CULL_FACE);
+	shader.setInt("reverse_normal", 1);
+	glBindVertexArray(cubeVAO);
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, floorTexture);
-	glDrawArrays(GL_TRIANGLES, 0, 6);
+	glBindTexture(GL_TEXTURE_2D, cubeTexture);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 	glBindVertexArray(0);
+	glEnable(GL_CULL_FACE);
+	shader.setInt("reverse_normal", 0);
 
 	modelRotAngle += deltaFrame * 50;
 	if (modelRotAngle >= 360) modelRotAngle = 0;
+	//绘制模型 缩放 旋转 平移
 	model = glm::mat4();
-	model = glm::translate(model, glm::vec3(-2.0f, 0.0f, 0.0f));
-	model = glm::rotate(model, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	model = glm::translate(model, glm::vec3(-4.0f, 0.0f, -4.0f));
 	model = glm::rotate(model, glm::radians(modelRotAngle), glm::vec3(0.0f, 1.0f, 0.0f));
 	model = glm::scale(model, glm::vec3(0.2f));
 	shader.setMatrix4("model", model);
 	nanosuit.Draw(shader);
 
 	model = glm::mat4();
-	model = glm::translate(model, glm::vec3(-1.5 - (sin(currentFrame) + 1) / 2, 2.0f, -1.0f));
-	model = glm::rotate(model, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-	model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	model = glm::translate(model, glm::vec3(-4.0f, -0.0f, 2.0f));
+	model = glm::scale(model, glm::vec3(0.2f));
+	shader.setMatrix4("model", model);
+	nanosuit.Draw(shader);
+
+	model = glm::mat4();
+	model = glm::translate(model, glm::vec3(4.0f, 0.0f, 2.0f));
 	model = glm::scale(model, glm::vec3(0.2f));
 	shader.setMatrix4("model", model);
 	nanosuit.Draw(shader);
@@ -212,11 +218,11 @@ void RenderLight(Shader shader) {
 	//绘制光源
 	glm::mat4 model = glm::mat4();
 	model = glm::translate(model, lightPos);
-	model = glm::scale(model, glm::vec3(0.2f));
+	model = glm::scale(model, glm::vec3(0.1f));
 	shader.setMatrix4("model", model);
 	shader.setVec3("lightColor", glm::vec3(1.0f));
 
-	glBindVertexArray(lightVAO);
+	glBindVertexArray(cubeVAO);
 	glDrawArrays(GL_TRIANGLES, 0, 36);
 	glBindVertexArray(0);
 }
@@ -253,77 +259,57 @@ int main(int argc, char** argv) {
 	glfwSetKeyCallback(window, key_click_callback);//键盘按下
 	glfwSetScrollCallback(window, scroll_callback);//鼠标滚轮
 
-	//光源立方体数据
-	float lightVertices[] = {
-		-0.5f, -0.5f, -0.5f,  
-		0.5f, -0.5f, -0.5f, 
-		0.5f,  0.5f, -0.5f, 
-		0.5f,  0.5f, -0.5f, 
-		-0.5f,  0.5f, -0.5f,  
-		-0.5f, -0.5f, -0.5f,  
-
-		-0.5f, -0.5f,  0.5f,  
-		0.5f, -0.5f,  0.5f, 
-		0.5f,  0.5f,  0.5f, 
-		0.5f,  0.5f,  0.5f, 
-		-0.5f,  0.5f,  0.5f,  
-		-0.5f, -0.5f,  0.5f,  
-
-		-0.5f,  0.5f,  0.5f,  
-		-0.5f,  0.5f, -0.5f,  
-		-0.5f, -0.5f, -0.5f,  
-		-0.5f, -0.5f, -0.5f,  
-		-0.5f, -0.5f,  0.5f,  
-		-0.5f,  0.5f,  0.5f,  
-
-		0.5f,  0.5f,  0.5f, 
-		0.5f,  0.5f, -0.5f,  
-		0.5f, -0.5f, -0.5f,  
-		0.5f, -0.5f, -0.5f,  
-		0.5f, -0.5f,  0.5f,  
-		0.5f,  0.5f,  0.5f,  
-
-		-0.5f, -0.5f, -0.5f,  
-		0.5f, -0.5f, -0.5f,  
-		0.5f, -0.5f,  0.5f,  
-		0.5f, -0.5f,  0.5f,  
-		-0.5f, -0.5f,  0.5f,  
-		-0.5f, -0.5f, -0.5f,  
-
-		-0.5f,  0.5f, -0.5f,  
-		0.5f,  0.5f, -0.5f,  
-		0.5f,  0.5f,  0.5f,  
-		0.5f,  0.5f,  0.5f,  
-		-0.5f,  0.5f,  0.5f,  
-		-0.5f,  0.5f, -0.5f 
+	//立方体数据
+	float CubeVertices[] = {
+		// Back face
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, // Bottom-left
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f, // top-right
+		0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, // bottom-right         
+		0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f,  // top-right
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f,  // bottom-left
+		-0.5f, 0.5f, -0.5f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f,// top-left
+		// Front face
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-left
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f,  // bottom-right
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,  // top-right
+		0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // top-right
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,  // top-left
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom-left
+		// Left face
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // top-right
+		-0.5f, 0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top-left
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,  // bottom-left
+		-0.5f, -0.5f, -0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-left
+		-0.5f, -0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f,  // bottom-right
+		-0.5f, 0.5f, 0.5f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // top-right
+		// Right face
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // top-left
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-right
+		0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top-right         
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,  // bottom-right
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,  // top-left
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom-left     
+		// Bottom face
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, // top-right
+		0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f, // top-left
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f,// bottom-left
+		0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, // bottom-left
+		-0.5f, -0.5f, 0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, // bottom-right
+		-0.5f, -0.5f, -0.5f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, // top-right
+		// Top face
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,// top-left
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom-right
+		0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, // top-right     
+		0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom-right
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,// top-left
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f // bottom-left   
 	};
 
-	glGenVertexArrays(1, &lightVAO);
-	glGenBuffers(1, &lightVBO);
-	glBindVertexArray(lightVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, lightVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(lightVertices), &lightVertices, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(lightVAO);
-
-	//地板矩形数据
-	float floorVertices[] = {
-		//pos normal texcoord
-		-8.0f, -8.0f, 0.0f, 0.0f, 0.f, 1.0f, 0.0f, 0.0f,
-		8.0f, -8.0f, 0.0f, 0.0f, 0.f, 1.0f, 8.0f, 0.0f,
-		-8.0f, 8.0f, 0.0f, 0.0f, 0.f, 1.0f, 0.0f, 8.0f,
-		-8.0f, 8.0f, 0.0f, 0.0f, 0.f, 1.0f, 0.0f, 8.0f,
-		8.0f, -8.0f, 0.0f, 0.0f, 0.f, 1.0f, 8.0f, 0.0f,
-		8.0f, 8.0f, 0.0f, 0.0f, 0.f, 1.0f, 8.0f, 8.0f
-	};
-
-	glGenVertexArrays(1, &floorVAO);
-	glGenBuffers(1, &floorVBO);
-	glBindVertexArray(floorVAO);
-	glBindBuffer(GL_ARRAY_BUFFER, floorVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(floorVertices), &floorVertices, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	glBindVertexArray(cubeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(CubeVertices), &CubeVertices, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(1);
@@ -331,65 +317,40 @@ int main(int argc, char** argv) {
 	glEnableVertexAttribArray(2);
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(floorVAO);
+	glBindVertexArray(cubeVAO);
 
-	//全屏矩形顶点数据
-	//float quadVertices[] = {
-	//	//位置             纹理坐标
-	//	-1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
-	//	-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-	//	1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-	//	1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-	//	1.0f, 1.0f, 0.0f, 1.0f, 1.0f,
-	//	-1.0f, 1.0f, 0.0f, 0.0f, 1.0f
-	//};
-
-	//unsigned int quadVAO, quadVBO;
-	//glGenVertexArrays(1, &quadVAO);
-	//glGenBuffers(1, &quadVBO);
-	//glBindVertexArray(quadVAO);
-	//glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-	//glEnableVertexAttribArray(0);
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-	//glEnableVertexAttribArray(1);
-	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	//glBindVertexArray(quadVAO);
-
-	//渲染深度贴图的帧缓冲
+	//生成渲染深度立方体贴图的帧缓冲
 	unsigned int depthMapFBO;
 	glGenFramebuffers(1, &depthMapFBO);
-
-	unsigned int depthMap;
-	glGenTextures(1, &depthMap);
-	glBindTexture(GL_TEXTURE_2D, depthMap);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, shadowWidth, shadowHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-	float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
-	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-	glBindTexture(GL_TEXTURE_2D, 0);
+	//通过几何着色器，1次渲染调用即可绘制出深度立方体贴图的6个面
+	//创建立方体贴图的6个面
+	unsigned int depthCubeMap;
+	glGenTextures(1, &depthCubeMap);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, depthCubeMap);
+	for (unsigned int i = 0; i < 6; i++) {
+		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_DEPTH_COMPONENT, shadowWidth, shadowHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	}
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
 	glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMap, 0);
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depthCubeMap, 0);
 	glDrawBuffer(GL_NONE);
 	glReadBuffer(GL_NONE);
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	floorTexture = loadTexture("resources/wood.png");
+	cubeTexture = loadTexture("resources/wood.png");
 
 	Shader lightShader("shaders/light.vs", "shaders/light.fs");
-	Shader blinnShader("shaders/blinn_phong.vs","shaders/blinn_phong.fs");
-	//Shader quadShader("shaders/quad.vs", "shaders/quad.fs");
-	Shader depthShader("shaders/depth.vs", "shaders/depth.fs");
+	Shader pointShadowShader("shaders/point_shadow.vs","shaders/point_shadow.fs");
+	Shader depthShader("shaders/depth.vs", "shaders/depth.fs", "shaders/depth.gs");
 
-	blinnShader.use();
-	blinnShader.setInt("material.texture_diffuse1", 0);
-	blinnShader.setInt("depthTexture", 3);
-	//quadShader.use();
-	//quadShader.setInt("texture1", 0);
+	pointShadowShader.use();
+	pointShadowShader.setInt("material.texture_diffuse1", 0);
+	pointShadowShader.setInt("depthTexture", 3);
 
 	nanosuit.LoadModel("models/nanosuit/nanosuit.obj");
 
@@ -403,50 +364,56 @@ int main(int argc, char** argv) {
 
 		//计算帧率
 		float frameRate = 1 / deltaFrame;
-		std::cout << "Current FPS: " << frameRate << std::endl;
+		//std::cout << "Current FPS: " << frameRate << std::endl;
 
 		processInput(window);
 		camera.Update(currentFrame, deltaFrame);
 
-		lightRotAngle += deltaFrame * 50;
-		if (lightRotAngle >= 360) lightRotAngle = 0;
-		glm::mat4 lightRotMat;
-		lightRotMat = glm::rotate(lightRotMat, glm::radians(lightRotAngle), glm::vec3(0.0f, 1.0f, 0.0f));
-		lightPos = lightRotMat * glm::vec4(lightSrcPos, 1.0);
-
-		//渲染深度贴图
+		//渲染深度立方体贴图
 		glViewport(0, 0, shadowWidth, shadowHeight);
 		glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
 		glClear(GL_DEPTH_BUFFER_BIT);
-		glm::mat4 lightView = glm::lookAt(lightPos, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		//正交投影通常用于定向光
-		glm::mat4 lightProjection = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, 0.1f, 100.0f);
-		///透视投影通常用于点光源和聚光灯
-		//glm::mat4 lightProjection = glm::perspective(45.0f, shadowWidth / shadowHeight, 0.1f, 100.0f);
-		glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+		float _near = 1.0f;
+		float _far = 25.0f;
+		glm::mat4 model;
+		glm::mat4 shadowProj = glm::perspective(glm::radians(90.0f), shadowWidth / shadowHeight, _near, _far);
+		std::vector<glm::mat4> shadowTransforms;
+		shadowTransforms.push_back(shadowProj * glm::lookAt(lightPos, lightPos + glm::vec3(1.0, 0.0, 0.0), glm::vec3(0.0, -1.0, 0.0)));
+		shadowTransforms.push_back(shadowProj * glm::lookAt(lightPos, lightPos + glm::vec3(-1.0, 0.0, 0.0), glm::vec3(0.0, -1.0, 0.0)));
+		shadowTransforms.push_back(shadowProj * glm::lookAt(lightPos, lightPos + glm::vec3(0.0, 1.0, 0.0), glm::vec3(0.0, 0.0, 1.0)));
+		shadowTransforms.push_back(shadowProj * glm::lookAt(lightPos, lightPos + glm::vec3(0.0, -1.0, 0.0), glm::vec3(0.0, 0.0, -1.0)));
+		shadowTransforms.push_back(shadowProj * glm::lookAt(lightPos, lightPos + glm::vec3(0.0, 0.0, 1.0), glm::vec3(0.0, -1.0, 0.0)));
+		shadowTransforms.push_back(shadowProj * glm::lookAt(lightPos, lightPos + glm::vec3(0.0, 0.0, -1.0), glm::vec3(0.0, -1.0, 0.0)));
+
 		depthShader.use();
-		depthShader.setMatrix4("lightSpaceMatrix", lightSpaceMatrix);
+		depthShader.setVec3("lightPos", lightPos);
+		depthShader.setFloat("far_plane", _far);
+		for (int i = 0; i < shadowTransforms.size(); i++) {
+			std::string paramName = std::string("shadowMatrices[" + std::to_string(i) + std::string("]"));
+			depthShader.setMatrix4(paramName.c_str(), shadowTransforms[i]);
+		}
+
 		RenderScene(depthShader, currentFrame);
 		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-		//用深度贴图渲染场景
+		//用深度立方体贴图渲染场景
 		glViewport(0, 0, screenWidth, screenHeight);
 		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		glm::mat4 view = glm::lookAt(camera.GetPos(), camera.GetPos() + camera.GetTarget(), camera.GetUp());
 		glm::mat4 projection = glm::perspective(camera.GetFov(), screenWidth / screenHeight, 0.1f, 100.0f);
-		blinnShader.use();
-		blinnShader.setMatrix4("view", view);
-		blinnShader.setMatrix4("projection", projection);
-		blinnShader.setMatrix4("lightSpaceMatrix", lightSpaceMatrix);
-		blinnShader.setVec3("viewPos", camera.GetPos());
-		blinnShader.setVec3("lightPos", lightPos);
-		blinnShader.setInt("blinn", blinn);
+		pointShadowShader.use();
+		pointShadowShader.setMatrix4("view", view);
+		pointShadowShader.setMatrix4("projection", projection);
+		pointShadowShader.setFloat("far_plane", _far);
+		pointShadowShader.setVec3("viewPos", camera.GetPos());
+		pointShadowShader.setVec3("lightPos", lightPos);
+		pointShadowShader.setInt("blinn", blinn);
 
 		glActiveTexture(GL_TEXTURE3);
-		glBindTexture(GL_TEXTURE_2D, depthMap);
-		RenderScene(blinnShader, currentFrame);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, depthCubeMap);
+
+		RenderScene(pointShadowShader, currentFrame);
 
 		//绘制光源
 		lightShader.use();
--- OpenGLTest/shaders/depth.fs
+++ OpenGLTest/shaders/depth.fs
@@ -1,6 +1,18 @@
 #version 330 core
 
+in vec4 FragPos;
+
+uniform vec3 lightPos;
+uniform float far_plane;
+
 void main()
 {
+	//计算片元和光源距离
+	float lightDistance = length(FragPos.xyz - lightPos);
 
+	//映射到[0,1]
+	lightDistance = lightDistance / far_plane;
+
+	//写入深度
+	gl_FragDepth = lightDistance;
 };
